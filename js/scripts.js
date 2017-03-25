ceba45a608c7cbc58cc5d1b624718c9182a95568
JanWrotkowski <trojmiasto2@wp.pl>
2017-03-25 23:35:21 +0100

    ...

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+// plik scripts.js
+
+/*
+var name = prompt('Wpisz swoje imię');
+*/
+//zakomentowalem pierwsza linijke bo mi sie nie podoba i nie chce zeby dzialala az do....
+//zmodyfikowania, hehe. teraz nie ma sily zeby sie sama otworzyla.
+alert('Witaj, ' + name);  // ten alert malo mi mowi, tzn usiluje wyciagnac ode mnie jak mam na imie.
+console.log('Witaj, ' + name);
